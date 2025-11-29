91fb0fdec21160db969f3adb6c0322768fb66849
Eladio Dev <Eladio.dev@gmail.com>
2025-11-28 23:23:34 -0300

    modificado el chat2

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // 1. Configuración para CALLAR las advertencias de Sass
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler', // Usa el compilador moderno y adiós alertas
+      },
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 1000, 
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('@n8n/chat')) {
+              return 'n8n-chat';
+            }
+            if (id.includes('react')) {
+              return 'react-vendor';
+            }
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
+});
